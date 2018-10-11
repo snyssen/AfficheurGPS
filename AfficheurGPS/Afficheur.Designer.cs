f8658886faf84f3e2cfd93528294c1135d787fe8
Simon Nyssen <36501744+snyssen@users.noreply.github.com>
2018-10-11 15:32:18 +0200

    Add project files.

---
+++ AfficheurGPS/Afficheur.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace AfficheurGPS
+{
+	partial class Afficheur
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.browser = new System.Windows.Forms.WebBrowser();
+			this.SuspendLayout();
+			// 
+			// browser
+			// 
+			this.browser.AllowWebBrowserDrop = false;
+			this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.browser.IsWebBrowserContextMenuEnabled = false;
+			this.browser.Location = new System.Drawing.Point(0, 0);
+			this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+			this.browser.Name = "browser";
+			this.browser.ScriptErrorsSuppressed = true;
+			this.browser.Size = new System.Drawing.Size(784, 561);
+			this.browser.TabIndex = 0;
+			// 
+			// Afficheur
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.ControlBox = false;
+			this.Controls.Add(this.browser);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Afficheur";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Afficheur";
+			this.TopMost = true;
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.Load += new System.EventHandler(this.Afficheur_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Afficheur_KeyDown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser browser;
+	}
+}
---
+++ AfficheurGPS/Afficheur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AfficheurGPS
+{
+	public partial class Afficheur : Form
+	{
+		public Afficheur()
+		{
+			InitializeComponent();
+			if (!WBEmulator.IsBrowserEmulationSet())
+			{
+				WBEmulator.SetBrowserEmulationVersion();
+			}
+			browser.Navigate("http://localhost/drupal");
+		}
+
+		private void Afficheur_Load(object sender, EventArgs e)
+		{
+			this.TopMost = true;
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.WindowState = FormWindowState.Maximized;
+		}
+
+		private void Afficheur_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				if (MessageBox.Show("Voulez-vous vraiment fermer l'application ?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+					this.Close();
+			}
+		}
+	}
+}
